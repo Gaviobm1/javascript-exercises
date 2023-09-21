2ff3192dee5e77f942e94e282bbeaf709e46f0db
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2023-09-21 16:24:33 +0200

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,34 @@
 const removeFromArray = function() {
+    if (arguments[1]) {
+     const index = arguments[0].indexOf(arguments[1]);
+     if (index > -1) {
+      arguments[0].splice(index, 1);
+     }
+            }
+     if (arguments[2]) {
+         const index = arguments[0].indexOf(arguments[2]);
+         if (index > -1) {
+          arguments[0].splice(index, 1);
+         }
+        }
+    if (arguments[3]) {
+            const index = arguments[0].indexOf(arguments[3]);
+            if (index > -1) {
+             arguments[0].splice(index, 1);
+            }
+        }
+    if (arguments[4]) {
+                const index = arguments[0].indexOf(arguments[4]);
+                if (index > -1) {
+                 arguments[0].splice(index, 1);
+                }
+            }
+                
+     return arguments[0]
 
-};
+    }
+    
+ 
 
 // Do not edit below this line
 module.exports = removeFromArray;
