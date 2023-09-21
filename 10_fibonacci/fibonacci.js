af8511cd8ab84c53b8dc504d13e828761ec39378
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2023-09-21 15:19:35 +0200

    Complete exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
 
-};
+const fibonacci = (index) => {
+    if (index < 0) {
+        return 'OOPS'
+    }
+    if (isNaN(index)) {
+        parseInt(index)
+    }
+const fib = [0, 1];
+
+for (let i = 2; i <= index; i++) {
+    fib[i] = fib[i-2] + fib [i-1]
+}
+
+return fib[index]
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
+
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const current = new Date()
+    const oldest = arr.sort((a, b) => {
+    const firstGuy = a.yearOfDeath - a.yearOfBirth || current.getFullYear() - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth || current.getFullYear() - b.yearOfBirth;
+    return firstGuy > nextGuy ? -1 : 1;
 
+   })
+return oldest[0];
 };
 
 // Do not edit below this line
-module.exports = findTheOldest;
+//module.exports = findTheOldest;
