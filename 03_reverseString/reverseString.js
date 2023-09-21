4459cc3199001fb90379a5e4c2577a54ca28f113
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2023-09-21 16:23:09 +0200

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
-};
+const reverseString = function(str) {
+    let newStr = str.split("");
+    newStr.reverse();
+    return newStr.join("");
+ };
 
 // Do not edit below this line
 module.exports = reverseString;
