081be18a7d0deaea4a99a894f0c3ffdf0aaeed7c
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2023-09-21 16:22:49 +0200

    Complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let str = "";
+    if (num>0){
+    
+        for (let i = num; i>0; i--) {
+                str += string ; 
+                }
+            }
+    if (num<0) {
+        str += "ERROR";
+                }
+    return str;
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = repeatString;
